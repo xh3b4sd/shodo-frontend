cad396e263e28b586a3ec9205dd46b83a4f9a2ea
Tim Schindler <xh3b4sd@gmail.com>
2021-11-28 14:56:32 +0100

    toggle token IDs on click

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import { render } from "react-dom";
-import { FixedSizeGrid as Grid } from 'react-window';
+import { BrowserRouter } from "react-router-dom";
+import { FixedSizeGrid } from 'react-window';
+import { Link } from "react-router-dom";
 import AutoSizer from "react-virtualized-auto-sizer";
 import InfiniteLoader from "react-window-infinite-loader";
 
@@ -9,11 +11,15 @@ const LOADING = 1;
 const LOADED = 2;
 let itemStatusMap = {};
 
-const isItemLoaded = index => !!itemStatusMap[index];
-const loadMoreItems = (startIndex, stopIndex) => {
+function isItemLoaded(index) {
+  return !!itemStatusMap[index];
+}
+
+function loadMoreItems(startIndex, stopIndex) {
   for (let index = startIndex; index <= stopIndex; index++) {
     itemStatusMap[index] = LOADING;
   }
+
   return new Promise(resolve =>
     setTimeout(() => {
       for (let index = startIndex; index <= stopIndex; index++) {
@@ -22,49 +28,108 @@ const loadMoreItems = (startIndex, stopIndex) => {
       resolve();
     }, 2500)
   );
-};
-
-const srcURL = (columnIndex, rowIndex) => {
-  let num = (rowIndex * 3) + columnIndex;
-  let str = "0000" + num;
-
-  str = str.slice(str.length - 4)
-
-  return "https://raw.githubusercontent.com/xh3b4sd/content/master/shodo/" + str + ".svg"
 }
 
-const Cell = ({ columnIndex, rowIndex, style }) => (
-  < div style={style} >
-    <img alt="" src={srcURL(columnIndex, rowIndex)} className="image"></img>
-  </div >
-);
+function newTokenID(col, row) {
+  let tid = (row * 6) + (col * 2);
+  let num = (row * 3) + col;
 
-const Example = () => (
-  <AutoSizer>
-    {({ height, width }) => (
-      <InfiniteLoader
-        isItemLoaded={isItemLoaded}
-        itemCount={100}
-        loadMoreItems={loadMoreItems}
-      >
-        {({ onItemsRendered, ref }) => (
-          <Grid
-            className="Grid"
-            columnCount={3}
-            columnWidth={width / 3}
-            height={height}
-            onItemsRendered={onItemsRendered}
-            ref={ref}
-            rowCount={100 / 3}
-            rowHeight={width / 3 / 0.5416248746}
-            width={width}
+  if (num % 2 !== 0) {
+    tid++;
+  }
+
+  return padTokenID(tid);
+}
+
+function padTokenID(tid) {
+  let str = "0000" + tid;
+  str = str.slice(str.length - 4);
+
+  return str;
+}
+
+function tokenIDFromSrc(src) {
+  let arr = src.split("/");
+  let tid = parseInt(arr[arr.length - 1], 10);
+
+  return tid;
+}
+
+function toggleTokenID(e) {
+  let tid = tokenIDFromSrc(e.target.src);
+
+  if (e.currentTarget.classList.contains("even")) {
+    e.currentTarget.classList.remove("even")
+    e.currentTarget.classList.add("uneven")
+    tid++;
+    e.target.src = newSrcURL(padTokenID(tid));
+  } else {
+    e.currentTarget.classList.remove("uneven")
+    e.currentTarget.classList.add("even")
+    tid--;
+    e.target.src = newSrcURL(padTokenID(tid));
+  }
+}
+
+function newSrcURL(tid) {
+  return "https://raw.githubusercontent.com/xh3b4sd/content/master/shodo/" + tid + ".svg"
+}
+
+function newCell({ columnIndex, rowIndex, style }) {
+  let add = "0x74fa...00f3"
+  let tid = newTokenID(columnIndex, rowIndex);
+
+  let className = "cell"
+  className += " "
+  if (tid % 2 === 0) {
+    className += "even"
+  } else {
+    className += "uneven"
+  }
+
+  return (
+    <div className={className} onClick={toggleTokenID} style={style}>
+      <div className="link-container view-token">
+        <Link className="link" to={"/" + tid}>view</Link>
+      </div>
+      <div className="link-container mint-token">
+        <Link className="link" to={"/" + add}>{add}</Link>
+      </div>
+      <img alt="" src={newSrcURL(tid)} className="image"></img>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <AutoSizer>
+        {({ height, width }) => (
+          <InfiniteLoader
+            isItemLoaded={isItemLoaded}
+            itemCount={100}
+            loadMoreItems={loadMoreItems}
           >
-            {Cell}
-          </Grid>
+            {({ onItemsRendered, ref }) => (
+              <FixedSizeGrid
+                className="grid"
+                columnCount={3}
+                columnWidth={width / 3}
+                height={height}
+                onItemsRendered={onItemsRendered}
+                ref={ref}
+                rowCount={100 / 3}
+                rowHeight={width / 3 / 0.5416248746}
+                width={width}
+              >
+                {newCell}
+              </FixedSizeGrid>
+            )}
+          </InfiniteLoader>
         )}
-      </InfiniteLoader>
-    )}
-  </AutoSizer>
-);
+      </AutoSizer>
+    </BrowserRouter>
+  );
+}
 
-render(<Example />, document.getElementById("root"));
+render(<App />, document.getElementById("root"));
