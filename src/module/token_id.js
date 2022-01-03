25350d87dce8c89175cf87c41ecd06adabee3744
xh3b4sd <xh3b4sd@gmail.com>
2022-01-03 13:46:19 +0100

    implement reverse grid order (#7)

--- src/module/token_id.js
+++ src/module/token_id.js
@@ -8,3 +8,14 @@ export function New(row, col, cnt) {
 
 	return tid;
 }
+
+export function Reverse(row, col, cnt, max) {
+	let tid = (row * cnt * 2) + (col * 2);
+	let num = (row * cnt) + col;
+
+	if (num % 2 !== 0) {
+		tid--;
+	}
+
+	return max - tid - 2;
+}
